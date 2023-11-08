527a79ef2bedd801ae40e2577528e4f1037797e0
Kevin Christian <kevin.christian@nostratech.com>
2023-11-08 21:21:52 +0700

    added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
+
+const dbConfig: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: 'db.sqlite',
+  entities: [User, Report],
+  synchronize: true,
+};
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dbConfig), UsersModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
